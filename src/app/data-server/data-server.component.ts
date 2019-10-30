7cbac1538b91470bd78e26827e9b3522e3f7cef4
revel8r <57109482+revel8r@users.noreply.github.com>
2019-10-30 18:05:51 -0400

    initial implementation of dataServer

--- src/app/data-server/data-server.component.ts
+++ src/app/data-server/data-server.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataServerComponent implements OnInit 
 {
+  landmarkLog = 'Landmark hits 1, 2, 3...';
+  
   constructor() 
   { }
 
