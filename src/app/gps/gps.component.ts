1530772d506acd31fc45b583130b44fefcf15abc
revel8r <57109482+revel8r@users.noreply.github.com>
2019-10-30 18:12:43 -0400

    initial implementation of gps component

--- src/app/gps/gps.component.ts
+++ src/app/gps/gps.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
-@Component({
+@Component(
+{
   selector: 'app-gps',
   templateUrl: './gps.component.html',
   styleUrls: ['./gps.component.css']
 })
-export class GpsComponent implements OnInit {
+export class GpsComponent implements OnInit 
+{
+  gpsLocationLatitude = 100;
+  gpsLocationLongitude = 200;
 
-  constructor() { }
+  constructor() 
+  { }
 
-  ngOnInit() {
+  ngOnInit() 
+  {
   }
-
 }
