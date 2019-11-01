b51e5ab25e2aa4087868d8ee0bd684f0c00f98e8
revel8r <57109482+revel8r@users.noreply.github.com>
2019-11-01 16:11:40 -0400

    Calling out to the rest service and retrieving data

--- src/app/data-server/data-server.component.ts
+++ src/app/data-server/data-server.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { RestService } from '../rest.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
 @Component({
   selector: 'app-data-server',
   templateUrl: './data-server.component.html',
@@ -9,11 +12,23 @@ export class DataServerComponent implements OnInit
 {
   landmarkLog = 'Landmark hits 1, 2, 3...';
   
-  constructor() 
-  { }
+  landmarks:any = [];
+
+  constructor(public rest:RestService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() 
   {
+    this.getLocalLandmarks(-77.128800, 38.852858, 5);
   }
 
+  getLocalLandmarks(latitude, longitude, distance)
+  {
+    console.log("Starting getLocalLandmarks");
+    this.landmarks = [];
+    this.rest.getLocalLandmarks(longitude, latitude, distance).subscribe((data: {}) => 
+    {
+      console.log(data);
+      this.landmarks = data;
+    });
+  }
 }
